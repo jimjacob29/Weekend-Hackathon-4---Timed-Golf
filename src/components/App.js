f82296e0afef93f1de531b31d903bfd5c631f950
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-11-07 22:58:12 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,79 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.timerId = undefined;
+    this.state = { time: 0, x: 0, y: 0, start: false };
+    this.handleStart = this.handleStart.bind(this);
+    this.handleMovement = this.handleMovement.bind(this);
+    this.gameWon = this.gameWon.bind(this);
   }
-  componentDidMount() {
-    
+
+  gameWon() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      return true;
+    }
+    return false;
   }
+
+  handleMovement(event) {
+    const keyPressed = event.key;
+    if (keyPressed === "ArrowRight") {
+      this.setState({ x: this.state.x + 5 });
+    } else if (keyPressed === "ArrowLeft") {
+      this.setState({ x: this.state.x - 5 });
+    } else if (keyPressed === "ArrowUp") {
+      this.setState({ y: this.state.y - 5 });
+    } else if (keyPressed === "ArrowDown") {
+      this.setState({ y: this.state.y + 5 });
+    }
+    if (this.gameWon()) {
+      document.removeEventListener("keydown", this.handleMovement);
+      clearInterval(this.timerId);
+    }
+  }
+
+  handleStart() {
+    this.setState({ start: true });
+    document.addEventListener("keydown", this.handleMovement);
+    this.timerId = setInterval(
+      () => this.setState({ time: this.state.time + 1 }),
+      1000
+    );
+  }
+
+  componentDidMount() {}
 
   componentWillUnmount() {
-    
+    clearInterval(this.timerId);
   }
 
-
+  initialRender() {
+    if (this.state.start) {
+      return (
+        <>
+          <div
+            className="ball"
+            style={{ left: this.state.x + "px", top: this.state.y + "px" }}
+          ></div>
+          <div className="hole"></div>
+          <div className="heading-timer">Time:{this.state.time}</div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="ball" style={{ left: "0px", right: "0px" }}></div>
+          <div className="hole"></div>
+          <button className="start" onClick={this.handleStart}>
+            start{" "}
+          </button>
+        </>
+      );
+    }
+  }
 
   render() {
-    return (
- <>
-</>
-    );
+    return <>{this.initialRender()}</>;
   }
 }
 
